2427e0d75f1fb5bc46b6255f9df94fe12deecc2e
anthonycaccese2 <anthony.m.caccese@gmail.com>
2024-01-14 11:59:39 -0500

    Started implementing read sensor data class

---
+++ WIldBlueberrySensorSystem/src/ReadSensorData/ReadSensorData.cpp
@@ -0,0 +1,50 @@
+#include "ReadSensorData.h"
+
+ReadSensorData::ReadSensorData(std::map<std::string, std::unique_ptr<Sensor>> *sensors)
+    : _sensors(sensors)
+{
+}
+
+ReadSensorData::~ReadSensorData()
+{
+}
+
+float ReadSensorData::getHarvesterLinearSpeedData()
+{
+    auto it = _sensors->find("harvester_linear_speed");
+    if (it != _sensors->end())
+    {
+        return it->second->readData();
+    }
+    return 0;
+}
+
+float ReadSensorData::getRakeRotationSpeedData()
+{
+    auto it = _sensors->find("rake_rotation_speed");
+    if (it != _sensors->end())
+    {
+        return it->second->readData();
+    }
+    return 0;
+}
+
+float ReadSensorData::getRakeHeightData()
+{
+    auto it = _sensors->find("rake_height");
+    if (it != _sensors->end())
+    {
+        return it->second->readData();
+    }
+    return 0;
+}
+
+float ReadSensorData::getBushHeightData()
+{
+    auto it = _sensors->find("bush_height");
+    if (it != _sensors->end())
+    {
+        return it->second->readData();
+    }
+    return 0;
+}

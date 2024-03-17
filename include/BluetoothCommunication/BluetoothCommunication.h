0e9e98c67c7f05094f425b79d4038df77d1619f8
Anthony Caccese <anthony.m.caccese@gmail.com>
2024-03-17 16:19:19 -0400

    Refactored the bluetooth Communication class for GoogleStyle

--- include/BluetoothCommunication/BluetoothCommunication.h
+++ include/BluetoothCommunication/BluetoothCommunication.h
@@ -1,27 +1,24 @@
-#ifndef BLUETOOTHCOMMUNICATION_H
-#define BLUETOOTHCOMMUNICATION_H
+#ifndef INCLUDE_BLUETOOTHCOMMUNICATION_BLUETOOTHCOMMUNICATION_H_
+#define INCLUDE_BLUETOOTHCOMMUNICATION_BLUETOOTHCOMMUNICATION_H_
 #include <ArduinoBLE.h>
 #include <vector>
 
-class BluetoothCommunication
-{
-public: 
-BluetoothCommunication();
-BluetoothCommunication(const char *deviceName, BLEService *serviceUUID);
-~BluetoothCommunication();
+class BluetoothCommunication{
+ public:
+    BluetoothCommunication();
+    BluetoothCommunication(const char *deviceName, BLEService *serviceUUID);
+    ~BluetoothCommunication();
 
-void addCharacteristicToList(BLEStringCharacteristic &characteristic);
-void addCharacteristicToList(const char* uuid, uint16_t permissions, int value);
-void begin();
+    void addCharacteristicToList(const char* uuid, uint16_t permissions, int value);
+    void begin();
 
-bool writeCharacteristic(BLEStringCharacteristic &characteristic, const char *data);
-bool writeCharacteristic(const char *uuid, const char *data);
-String receivedDataCharacteristic(const char *uuid, char *buffer, int length);
+    bool writeCharacteristic(const char *uuid, const char *data);
+    String receivedDataCharacteristic(const char *uuid, char *buffer, int length);
 
-BLEService *service;
-BLEDevice *central;
-std::vector<BLEStringCharacteristic> characteristics;
-private: 
-const char *_deviceName = "Arduino";
+    BLEService *service;
+    BLEDevice *central;
+    std::vector<BLEStringCharacteristic> characteristics;
+ private:
+    const char *_deviceName = "Arduino";
 };
-#endif
+#endif  // INCLUDE_BLUETOOTHCOMMUNICATION_BLUETOOTHCOMMUNICATION_H_
--- src/BluetoothCommunication/BluetoothCommunication.cpp
+++ src/BluetoothCommunication/BluetoothCommunication.cpp
@@ -1,35 +1,22 @@
 #include "./BluetoothCommunication/BluetoothCommunication.h"
 
-BluetoothCommunication::BluetoothCommunication(){
+BluetoothCommunication::BluetoothCommunication() {
 }
 
-BluetoothCommunication::BluetoothCommunication(const char *deviceName, BLEService *serviceUUID){
+BluetoothCommunication::BluetoothCommunication(const char *deviceName, BLEService *serviceUUID) {
   if (!BLE.begin()) {
     Serial.println("Starting BLE failed!");
-    while (1);
+    while (1) {}
   }
   _deviceName = deviceName;
   service =  serviceUUID;
 }
 
-BluetoothCommunication::~BluetoothCommunication(){
+BluetoothCommunication::~BluetoothCommunication() {
 }
 
-
-void BluetoothCommunication::addCharacteristicToList(BLEStringCharacteristic &characteristic){
-  characteristics.push_back(characteristic);
-  if (*service) {
-        service->addCharacteristic(characteristic);
-    } else {
-        // Handle the case where 'service' is not properly initialized
-        // Log an error or take appropriate action
-        Serial.println("ERROR -- adding characteristic failed");
-    }
-}
-
-void BluetoothCommunication::addCharacteristicToList(const char* uuid, uint16_t permissions, int value){
+void BluetoothCommunication::addCharacteristicToList(const char* uuid, uint16_t permissions, int value) {
   BLEStringCharacteristic characteristic =  BLEStringCharacteristic(uuid, permissions, value);
-  
   characteristics.push_back(characteristic);
   if (service) {
         service->addCharacteristic(characteristic);
@@ -39,48 +26,41 @@ void BluetoothCommunication::addCharacteristicToList(const char* uuid, uint16_t 
         Serial.println("ERROR -- adding characteristic failed");
     }
 }
-void BluetoothCommunication::begin(){
+void BluetoothCommunication::begin() {
   if (!BLE.begin()) {
     Serial.println("Starting BLE failed!");
-    while (1);
+    while (1) {}
   }
-  
-  // // set local name
+  // set local name
   BLE.setLocalName(_deviceName);
   Serial.println("Setting up BLE...");
   BLE.setAdvertisedService(*service);
   Serial.println(service->uuid());
-  if(characteristics.size() <1){
+  if (characteristics.size() <1) {
     Serial.println("ERROR ---- No BLE characteristics setup");
     return;
   }
   BLE.addService(*service);
-
   // // Start advertising
   BLE.advertise();
   Serial.println("BLE device advertising...");
-
 }
 
-bool BluetoothCommunication::writeCharacteristic(BLEStringCharacteristic &characteristic, const char *data){
-  return (characteristic.writeValue(data) ? true : false);
-}
-bool BluetoothCommunication::writeCharacteristic(const char *uuid, const char *data){
+bool BluetoothCommunication::writeCharacteristic(const char *uuid, const char *data) {
   for (auto& characteristic : characteristics) {
-    if (characteristic.uuid() == uuid){
+    if (characteristic.uuid() == uuid) {
       return (characteristic.writeValue(data) ? true : false);
     }
   }
   return false;
 }
 
-String BluetoothCommunication::receivedDataCharacteristic(const char *uuid, char *buffer, int length){
-  
+String BluetoothCommunication::receivedDataCharacteristic(const char *uuid, char *buffer, int length) {
   for (auto& characteristic : characteristics) {
-    if (characteristic.uuid() == uuid){
+    if (characteristic.uuid() == uuid) {
       if (characteristic.written()) {
         // Read the data
-        characteristic.readValue(buffer,length);
+        characteristic.readValue(buffer, length);
         Serial.print("Received value: ");
         Serial.println(buffer);
         return buffer;
